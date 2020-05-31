94d72ed535d1cbbca47bc39bd3262423a8c0443a
Rayanne Barbosa <rayannelisse@hotmail.com>
2020-05-31 02:05:18 -0300

    Criacao dos scripts de Banco

---
+++ Banco/Scripts.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE transp;
+
+CREATE USER 'hive'@'%' IDENTIFIED BY '123';
+
+GRANT ALL ON *.* TO 'hive'@'%' WITH GRANT OPTION;
+
+flush privileges;
+
+USE transp;
+
+/***** TABELA TRANSPORTADORAS *****/
+CREATE TABLE tb_transportadoras (
+  id int auto_increment primary key,
+  nome varchar(100),
+  email varchar (100),
+  empresa varchar(100),
+  telefone varchar(30),
+  celular varchar(30),
+  whatsapp varchar(30),
+  modal varchar (100),
+  cep varchar(100),
+  estado varchar (2),
+  cidade varchar (100),
+  bairro varchar (100),
+  endereco varchar (255),
+  numero int  
+);
+/*****************/
